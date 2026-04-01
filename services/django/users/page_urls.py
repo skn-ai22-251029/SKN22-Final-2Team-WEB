3c001df9d91fe3e1fd299d6734775d905b89d30f
harry1749 <haryeonghwang@gmail.com>
2026-04-01 10:41:38 +0900

    feat(django): 관리자 로그인 진입과 시연용 화면 추가

--- services/django/users/page_urls.py
+++ services/django/users/page_urls.py
@@ -4,6 +4,7 @@ from . import page_views
 urlpatterns = [
     path("", page_views.home, name="home"),
     path("login/", page_views.login_view, name="login"),
+    path("vendor/login/", page_views.vendor_login_view, name="vendor-login"),
     path("signup/", page_views.signup_view, name="signup"),
     path("logout/", page_views.logout_view, name="logout"),
     path("profile/", page_views.profile_view, name="profile"),
