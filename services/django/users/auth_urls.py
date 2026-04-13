3755f7704c275b31a3354c9adfddb5f1164edc34
harry1749 <haryeonghwang@gmail.com>
2026-04-13 15:00:48 +0900

    회원가입 auth 경로를 노출한다

--- services/django/users/auth_urls.py
+++ services/django/users/auth_urls.py
@@ -4,9 +4,11 @@ from .views import (
     AuthLoginView,
     AuthLogoutView,
     AuthWithdrawView,
+    RegisterView,
 )
 
 urlpatterns = [
+    path("register/", RegisterView.as_view(), name="auth-register"),
     path("login/", AuthLoginView.as_view(), name="auth-login"),
     path("logout/", AuthLogoutView.as_view(), name="auth-logout"),
     path("withdraw/", AuthWithdrawView.as_view(), name="auth-withdraw"),
